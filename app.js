edbba7abfcf45c80daad5c3ba6cc0e53e7189909
Jack Sunderman <jacksunderman@Jacks-MacBook-Air.local>
2023-08-24 13:57:07 -0400

    guided practice done

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const container = document.querySelector('.container');
-const colors = ['red', 'blue', 'green'];
-const sizes = ['sm', 'md', 'lg'];
+const colors = ['red', 'blue', 'green', 'yellow'];
+const sizes = ['sm', 'md', 'lg', 'xl'];
+const types = ['square', 'circle']
 
 function randomShape(){
   const colorIdx = Math.floor(Math.random()*colors.length);
   const sizeIdx = Math.floor(Math.random()*sizes.length);
+  const typeIdx = Math.floor(Math.random()* types.length);
   const shape = {
     color: colors[colorIdx],
-    size: sizes[sizeIdx]
+    size: sizes[sizeIdx],
+    type: types[typeIdx]
 
   };
   return shape;
@@ -19,15 +22,19 @@ const shapes = [
 ];
 
 function render(){
-  const html = shapes.map(function(shape){
-    return `<div class='${shape.color} ${shape.size}'></div>`;
+  const html = shapes.map(function(shape, idx){
+    return `<div class='${shape.color} ${shape.size} ${shape.type}'>${idx}</div>`;
   });
   container.innerHTML = html.join('');
 }
 
 render();
-setInterval(function(){
+
+const interval = setInterval(function(){
   const newShape = randomShape();
   shapes.push(newShape);
   render();
+  if (shapes.length === 20){
+  clearInterval(interval);
+}
 }, 500);
